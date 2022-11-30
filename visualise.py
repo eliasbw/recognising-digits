31cd81d57a1d7caf3acd9bdca42d76a10a3b23dc
Elias Nilsson <elianils@student.chalmers.se>
2022-11-30 21:23:50 +0100

    Fixed bug

--- visualise.py
+++ visualise.py
@@ -2,23 +2,22 @@ from matplotlib import pyplot as plt
 from matplotlib.patches import Patch
 from matplotlib.colors import ListedColormap
 
-from logic import train
+from hopfield_logic import train
 
 
-def visualise(pattern_from_gui, colors):
+def generate_figures(pattern_from_gui, colors):
     cmap = ListedColormap(colors)
-
+    patches = [
+        Patch(color=colors[i], label="Zeros" if i == 0 else "Ones") for i in [0, 1]
+    ]
 
     plt.imshow(pattern_from_gui, cmap=cmap)
-    patches = [Patch(color=colors[i], label="Zeros" if i == 1 else "Ones") for i in [0, 1]]
-    plt.legend(handles=patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    plt.legend(handles=patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.0)
     plt.title("The input data")
     plt.show()
 
     iterated_pattern = train(pattern_from_gui)
-
     plt.imshow(iterated_pattern, cmap=cmap)
     plt.title("The pattern found")
-    patches = [Patch(color=colors[i], label="Zeros" if i == 1 else "Ones") for i in [0, 1]]
-    plt.legend(handles=patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    plt.legend(handles=patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.0)
     plt.show()
